ee64a1d91e399a657893ecda3c573422df6e633e
Prithviraj <prithvi@kissflow.com>
2023-07-10 23:51:07 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("test action")
+  console.log("test action 1")
   return (
     <div className="App">
       <header className="App-header">
