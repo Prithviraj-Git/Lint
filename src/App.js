a458bdfe5a324149a7c00d4495413e0cfae97c4d
Prithviraj <prithvi@kissflow.com>
2023-07-19 12:05:07 +0530

    lint test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("test action 5")
+  console.log("test action 6")
   return (
     <div className="App">
       <header className="App-header">
